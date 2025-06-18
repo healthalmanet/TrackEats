6ae684ff0b33498d38e9ed986055453feb05d250
imran khan <imrankhan.ca2020@gmail.com>
2025-06-19 00:29:05 +0530

    code deleted

--- backend/app/ml_diet/t.py
+++
@@ -1,9 +0,0 @@
-from predict import recommend_meals
-
-class UserProfile:
-    goal = "Lose Weight"
-    diet_type = "Vegetarian"
-    health_conditions = ["diabetes"]
-
-result = recommend_meals(UserProfile())
-print(result)
